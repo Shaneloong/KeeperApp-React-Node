d56922f142770092af809ef102712bd55c4453b9
Shane Loong <messixuan6636@gmail.com>
2022-04-30 16:59:58 +0800

    Updated Note Style

--- keeper-app/src/components/Note.jsx
+++ keeper-app/src/components/Note.jsx
@@ -10,7 +10,7 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>
+      <button style={{ borderRadius: "50%" }} onClick={handleClick}>
         <DeleteIcon />
       </button>
     </div>
